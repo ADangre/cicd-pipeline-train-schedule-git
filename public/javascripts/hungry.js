07f50190bd3d4cae32961f8d62e4cb3dbfb36a95
Amit Dangre <amit.dangre@gmail.com>
2024-05-15 06:53:12 +0200

    first commit for hungry js

---
+++ public/javascripts/hungry.js
@@ -0,0 +1,14 @@
+function populateTrains() {
+    var trainsContent = '';
+  
+    $.getJSON( '/trains', function( data ) {
+  
+        trains = data
+  
+      $.each(data, function(index){
+        trainsContent += '<div class="p-2"><a href="#" class="linkshowtrain" rel="' + index + '">' + this.name + '</a></div>';
+      });
+      $('#trains').html(trainsContent);
+      $('#trains').on('click', 'div a.linkshowtrain', showTrainInfo);
+    });
+  };
